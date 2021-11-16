0554a3ab34b7007b2a32fb72157b3fed2c1a398e
Joshua F. Wiley, Ph.D <jwiley.psych@gmail.com>
2021-11-16 16:20:33 +1100

    moving tab2mat to its own file

--- src/integratere.cpp
+++ src/integratere.cpp
@@ -1,31 +1,9 @@
 #include <RcppArmadillo.h>
 #include "integratemvn.h"
+#include "tab2mat.h"
 // [[Rcpp::depends(RcppArmadillo)]]
 
 using namespace Rcpp;
-
-//' Convert a row of a table to a matrix
-//'
-//' TODO: write description.
-//'
-//' @param X a matrix
-//' @return A numeric matrix with one row.
-//' @export
-//' @examples
-//'
-// [[Rcpp::export]]
-arma::mat tab2mat(const arma::mat& X) {
-  int ncol = X.n_cols;
-  double dims = sqrt(ncol); 
-  arma::mat Z = arma::zeros(dims, dims);
-  
-  for (int i = 0; i < dims; i++) {
-    for (int j = 0; j < dims; j++) {
-      Z(i, j) = X(0, j + dims * i);
-    }
-  }
-  return(Z);
-}
 
 //' Integrate over Random Effects
 //'
---
+++ src/tab2mat.h
@@ -0,0 +1,9 @@
+#ifndef __tab2mat__
+#define __tab2mat__
+
+#include <RcppArmadillo.h>
+arma::mat tab2mat(const arma::mat& X);
+
+#endif // __tab2mat__
+
+
